fe1e3b69789a7d0ccd5c9a07f6fdfd94eb92df55
Jorge Arteaga <jarteaga@grupopaz.com.bo>
2020-04-01 18:00:40 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+style = () =>{
+    return gulp.src([
+        'node_modules/bootstrap/scss/bootstrap.scss', 
+        'scss/style.scss'
+        ])
+    .pipe(sass())     
+    .pipe(gulp.dest('./css'))     
+    .pipe(browserSync.stream());
+};
+
+js = () => {
+    return gulp.src([
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/popper.js/dist/popper.min.js',
+        'node_modules/jquery/dist/jquery.min.js'
+        ])
+    .pipe(gulp.dest('./js'))
+    .pipe(browserSync.stream());
+};
+
+watch = () =>{
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    });
+    gulp.watch('./scss/*.scss', style);
+    gulp.watch('./*html').on('change', browserSync.reload);
+};
+
+gulp.task('build', 
+    gulp.parallel(style, js))
+
+gulp.task('default',
+    gulp.series('build', watch))
